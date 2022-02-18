99297dc31be68f6f3bc387583c47116d54741137
chenjinsongcjs <13887096914@163.com>
2022-02-18 15:33:39 +0800

    动态数组完成且测试通过

--- src/com/cjs/test/Test.java
+++ src/com/cjs/test/Test.java
@@ -1,8 +1,38 @@
 package com.cjs.test;
 
+import com.cjs.arrays.Array;
+
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("test...");
+        Test test = new Test();
+        test.testArray();
+    }
+    //动态数组测试
+    private void testArray(){
+        Array<Integer> arr = new Array<>();
+        for(int i = 0 ; i < 10 ; i ++)
+            arr.addLast(i);
+        System.out.println(arr);
+
+        arr.add(1, 100);
+        System.out.println(arr);
+
+        arr.addFirst(-1);
+        System.out.println(arr);
+
+        arr.remove(2);
+        System.out.println(arr);
+
+        arr.removeElement(4);
+        System.out.println(arr);
+
+        arr.removeFirst();
+        System.out.println(arr);
+
+        for(int i = 0 ; i < 4 ; i ++){
+            arr.removeFirst();
+            System.out.println(arr);
+        }
     }
 }
